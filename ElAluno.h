4973115e6b59e044f55054e0166041ffd4fabfb0
Leonardo2 <leo.decristo@hotmail.com>
2024-07-15 19:52:09 -0300

    Implementando ElAluno

--- ElAluno.h
+++ ElAluno.h
@@ -1,1 +1,24 @@
+#pragma once
 
+#include "stdafx.h"
+#include "Aluno.h"
+
+class ElAluno
+{
+    private:
+        Aluno* pAl;
+        ElAluno* pNext;
+        ElAluno* pPrev;
+
+    public:
+        ElAluno(Aluno*);
+        ElAluno();
+        ~ElAluno();
+
+        void setAluno (Aluno* aluno);
+        Aluno* getAluno ();
+        void setNext (ElAluno* next);
+        ElAluno* getNext ();
+        void setPrev (ElAluno* prev);
+        ElAluno* getPrev ();
+};
